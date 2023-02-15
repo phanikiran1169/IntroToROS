c71329a97e8d8826cbd93a7c8e7c32ae7884cb5f
saichand44 <saichandgubbala@gmail.com>
2023-02-16 00:34:21 +0530

    using global variable

--- S3_Communications_using_topics/solution_exercise1/src/pubvelsafe.cpp
+++ S3_Communications_using_topics/solution_exercise1/src/pubvelsafe.cpp
@@ -6,64 +6,27 @@
 #include <stdlib.h> // For rand() and RAND_MAX
 #include <iomanip> // for std::setprecision and std::fixed
 
-/**
-* A callback function to publish angular and linear velocities.
-* The velocities are published in two patterns:
-* 1. A random angular velocity command and a fixed linear velocity of 1.0 
-*    when the turtle is placed in a safe zone defined by a square around
-*    the center of the window.
-* 2. A random angular velocity and a random linear velocity otherwise.
-*
-* Size of safe zone considered: 200 x 200 pixels
-*/
+// Declare global postion variables
+float position_x;
+float position_y;
 
-void publishVelocities(const turtlesim::Pose& pose_msg, ros::Publisher& pub)
+// Declare global variable for checking callback function exceution
+bool callback_trigger = false;
+
+// A callback function.  Executed each time a new pose
+// message arrives.
+void poseMessageReceived(const turtlesim::Pose& msg) 
 {
   ROS_INFO_STREAM(std::setprecision(2) << std::fixed
-    << "current_position=(" <<  pose_msg.x << "," << pose_msg.y << ")");
-
-  // Seed the random number generator.
-  srand(time(0));
-
-  // Loop at 2Hz until the node is shut down.
-  ros::Rate rate(2);
-
-  while(ros::ok())
-    // Create and fill in the message.  The other four
-    // fields, which are ignored by turtlesim, default to 0.
-    geometry_msgs::Twist motion_msg;
-
-  {
-    if (pose_msg.x < 200 && pose_msg.y < 200)
-    {
-    ROS_INFO_STREAM("Turtle is inside safe zone");
-      motion_msg.linear.x = 1.0;
-      motion_msg.angular.z = 2*double(rand())/double(RAND_MAX) - 1;
-    }
-    else
-    {
-    ROS_INFO_STREAM("Turtle is outside safe zone");
-      motion_msg.linear.x = double(rand())/double(RAND_MAX);
-      motion_msg.angular.z = 2*double(rand())/double(RAND_MAX) - 1;
-    }
-
-    // Publish the message.
-    pub.publish(msg);
-
-    // Send a message to rosout with the details.
-    ROS_INFO_STREAM("Sending random velocity command:"
-      << " linear=" << motion_msg.linear.x
-      << " angular=" << motion_msg.angular.z);
-
-    // Wait until it's time for another iteration.
-    rate.sleep();
-  }
-  
+    << "current_position=(" <<  msg.x << "," << msg.y << ")");
+  position_x = msg.x;
+  position_y = msg.y;
+  callback_trigger = true;
 }
 
 int main(int argc, char **argv)
 {
-  // Initialize the ROS system and create publisher and subscriber node.
+  // Initialize the ROS system and become a node.
   ros::init(argc, argv, "publish_velocity_safe");
   ros::NodeHandle nh;
 
@@ -72,9 +35,56 @@ int main(int argc, char **argv)
     "turtle1/cmd_vel", 1000);
 
   // Create a subscriber object.
-  ros::Subscriber sub = nh.subscribe("turtle1/pose", 1000, 
-                                    boost::bind(publishVelocities, _1, pub));
+  ros::Subscriber sub = nh.subscribe("turtle1/pose", 1000,
+    &poseMessageReceived);
 
-  // Let ROS take over.
-  ros::spin();
+  // Seed the random number generator.
+  srand(time(0));
+
+  // Loop at 2Hz until the node is shut down.
+  ros::Rate rate(10);
+
+  while(ros::ok())
+  {
+    // Create and fill in the message.  The other four
+    // fields, which are ignored by turtlesim, default to 0.
+    geometry_msgs::Twist motion_msg;
+
+    // Let ROS take over
+    while(true)
+    {
+      ros::spinOnce();
+
+      if (callback_trigger == true)
+      {
+        break;
+      }
+    }
+
+    if (position_x < 200 && position_y < 200)
+    {
+      ROS_INFO_STREAM("Turtle is inside safe zone");
+      motion_msg.linear.x = 1.0;
+      motion_msg.angular.z = 2*double(rand())/double(RAND_MAX) - 1;
+    }
+    else
+    {
+      ROS_INFO_STREAM("Turtle is outside safe zone");
+      motion_msg.linear.x = double(rand())/double(RAND_MAX);
+      motion_msg.angular.z = 2*double(rand())/double(RAND_MAX) - 1;
+    }
+
+    // Publish the message.
+    pub.publish(motion_msg);
+
+    // Send a message to rosout with the details.
+    ROS_INFO_STREAM("Sending random velocity command:"
+      << " linear=" << motion_msg.linear.x
+      << " angular=" << motion_msg.angular.z);
+
+    callback_trigger = false;
+
+    // Wait until it's time for another iteration.
+    rate.sleep();
+  }
 }
